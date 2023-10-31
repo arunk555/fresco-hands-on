2cfecb5db369f3526f988b33f5c07b7c9724d9e9
arunk555 <arunkurusev@gmail.com>
2023-10-31 17:29:09 +0000

    latest changes

--- app.js
+++ app.js
@@ -8,4 +8,17 @@ app.use(express.json());
 
 //write the code for routes here
 
+app.get("/view", (req, res) => {
+  const id = req.query.id ?? "";
+  fs.readFile("./get.json", (err, data) => {
+    if (err) throw err;
+    if (id === "") {
+      res.status(200).json(JSON.parse(data));
+    } else {
+      const flt = JSON.parse(data).filter((item) => item.id === id);
+      res.status(200).json(flt);
+    }
+  });
+});
+
 module.exports = app;
